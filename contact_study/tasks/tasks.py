058ffc0b962c5697c128ef5f38731dfb4cf5279f
Kyle Golobish <kyle.golobish@yale.edu>
2026-05-21 21:57:35 -0400

    fixing the freejoint location and pose

--- contact_study/tasks/tasks.py
+++ contact_study/tasks/tasks.py
@@ -234,13 +234,13 @@ class GraspReorientTask(BaseTask):
             max_steps         = 100,
             success_threshold = 0.001,  # combined pose error
             cost_weights      = {
-                "w_quat": 0.0, #0.5, 
-                "w_pos": 0.0,#0.1, 
+                "w_quat": 0.05, #0.5, 
+                "w_pos": 0.01,#0.1, 
                 "w_contact": 5.0,#0.5, 
                 "w_joint": 0.1, 
-                "w_fallen": 0.0,#50.0,
-                "w_quat_term": 0.0,#10.0, 
-                "w_pos_term": 0.0,#5.0, 
+                "w_fallen": 50.0,#50.0,
+                "w_quat_term": 10.0,#10.0, 
+                "w_pos_term": 10.0,#5.0, 
                 "w_fallen_term": 0.0,#100.0
             }
         )
