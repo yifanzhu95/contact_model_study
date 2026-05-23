939b6fbdea70b9130f6cc3dc437bd14e1a937ee8
Kyle Golobish <krgolobish@gmail.com>
2026-05-23 11:34:01 -0400

    added a way to set a camera in the run_viewer_and_print_state and added a camera to the xml

--- scenes/run_viewer_and_print_state.py
+++ scenes/run_viewer_and_print_state.py
@@ -1,6 +1,8 @@
 import time
 import mujoco
 import mujoco.viewer
+import numpy as np
+
 
 # Load a built-in sample model (Humanoid) that contains pre-saved keyframes
 model = mujoco.MjModel.from_xml_path("scenes/leap_hand/scene_leap_cube.xml")
@@ -39,6 +41,33 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             "Qvel:",data.qvel
         ) 
 
+        cam = viewer.cam
+        
+        az = np.radians(cam.azimuth)
+        el = np.radians(cam.elevation)
+        
+        forward = np.array([
+            np.sin(az) * np.cos(el),
+            -np.cos(az) * np.cos(el),
+            np.sin(el)
+        ])
+        
+        pos = cam.lookat - cam.distance * forward
+        
+        world_up = np.array([0, 0, 1])
+
+        if np.abs(np.dot(forward, world_up)) > 0.9998:
+            right = np.array([1, 0, 0]) if forward[2] < 0 else np.array([-1, 0, 0])
+        else:
+            right = np.cross(forward, world_up)
+            right /= np.linalg.norm(right)
+        up = np.cross(right, forward)
+        up /= np.linalg.norm(up)
+
+        pos_str = f"{pos[0]:.4f} {pos[1]:.4f} {pos[2]:.4f}"
+        xyaxis_str = f"{right[0]:.4f} {right[1]:.4f} {right[2]:.4f} {up[0]:.4f} {up[1]:.4f} {up[2]:.4f}"
+                
+        print(f'<camera name="exported_cam" pos="{pos_str}" xyaxis="{xyaxis_str}" />')
         # 4. Sync the viewer
         viewer.sync()
 
