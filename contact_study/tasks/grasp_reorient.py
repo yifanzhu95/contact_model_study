50b05a980d41623732ca5c348d8a7e89468fa2e1
Kyle Golobish <krgolobish@gmail.com>
2026-07-14 16:37:24 -0400

    comminting for merge

--- contact_study/tasks/grasp_reorient.py
+++ contact_study/tasks/grasp_reorient.py
@@ -263,8 +263,8 @@ class GraspReorientTask(BaseTask):
                 "w_quat": 75.0, #5.0
                 "w_pos": 400.0, #40.0
                 "w_velo": 0.0,
-                "w_contact": 30.0,#250.0,#500.0,#2.5
-                "w_joint": 3.0, #0.1
+                "w_contact": 750.0,#250.0,#500.0,#2.5
+                "w_joint": 6.0, #0.1
                 "w_joint_velo": 0.0,
                 "w_fallen": 300.0, #30.0,
                 "w_quat_term": 100.0, #10.0
