596f88ce3acf3e650d0b70c538344441a050e61b
Kyle Golobish <krgolobish@gmail.com>
2026-07-17 11:41:50 -0400

    commitng for merge

--- contact_study/tasks/grasp_reorient.py
+++ contact_study/tasks/grasp_reorient.py
@@ -261,11 +261,11 @@ class GraspReorientTask(BaseTask):
             max_steps          = 500,
             success_thresholds = {"pos": 0.02, "quat": 0.04, "vel": 0.1},
             cost_weights       = {
-                "w_quat": 75.0, #5.0
+                "w_quat": 100.0, #5.0
                 "w_pos": 400.0, #40.0
                 "w_velo": 0.0,
                 "w_contact": 750.0,#250.0,#500.0,#2.5
-                "w_joint": 6.0, #0.1
+                "w_joint": 4.0, #0.1
                 "w_joint_velo": 0.0,
                 "w_fallen": 300.0, #30.0,
                 "w_quat_term": 100.0, #10.0
