a9a11e07a4f729acd1d28e2619f754ef9e6d377b
Kyle Golobish <krgolobish@gmail.com>
2026-07-29 14:56:56 -0400

    fixed cntrl freq cell, didnt have warms start turned on

--- experiments/hpc/run_cntrl_freq_cell.py
+++ experiments/hpc/run_cntrl_freq_cell.py
@@ -90,7 +90,7 @@ def run_cell(args):
             temperature    = args.temperature,
             noise_sigma    = args.noise_sigma,
             substeps       = args.substeps,
-            warm_start     = True,
+            warm_start     = False,
             resample_interval = 1,
             use_full_graph = args.use_full_graph,
             delta_range    = (-args.delta, args.delta),
@@ -161,7 +161,7 @@ def build_parser() -> argparse.ArgumentParser:
     p.add_argument("--nconmax",       type=int,   default=200)
     p.add_argument("--njmax",         type=int,   default=500)
     p.add_argument("--use_full_graph",
-                   action=argparse.BooleanOptionalAction, default=False)
+                   action=argparse.BooleanOptionalAction, default=True)
     p.add_argument("--seed",          type=int,   default=None)
     p.add_argument("--debug",         action="store_true")
     return p
